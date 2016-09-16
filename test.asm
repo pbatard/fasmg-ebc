9f1fc6d3abe996348f9a5fc637d866ff94c637b8
Pete Batard <pete@akeo.ie>
2016-09-16 13:27:20 +0100

    fix carry over of indexes from sub structures

--- test.asm
+++ test.asm
@@ -12,7 +12,7 @@ entry EfiMain
 section '.text' code executable readable
 EfiMain:
   MOV R1, @R1(EFI_SYSTEM_TABLE.Hdr)
-  MOV R1, @R1(EFI_TABLE_HEADER.HeaderSize)
+  MOV R1, @R1(EFI_SYSTEM_TABLE.FirmwareVendor)
   MOV R1, @R1(EFI_TABLE_HEADER.Reserved)
   MOV R1, @R1(EFI_SYSTEM_TABLE.ConsoleInHandle)
   MOV R1, @R1(EFI_SYSTEM_TABLE.ConOut)
